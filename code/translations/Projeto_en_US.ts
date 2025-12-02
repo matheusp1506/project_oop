f89fb006419a49bd0b9cbb94d8dc3aa9fb2fb8d4
matheusp1506 <matheusp1506@gmail.com>
2025-12-02 12:10:03 -0300

    Added the remaining files

---
+++ code/translations/Projeto_en_US.ts
@@ -0,0 +1,81 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainMenu</name>
+    <message>
+        <location filename="../mainmenu.cpp" line="16"/>
+        <location filename="../mainmenu.cpp" line="46"/>
+        <source>New Game</source>
+        <translation>New Game</translation>
+    </message>
+    <message>
+        <location filename="../mainmenu.cpp" line="17"/>
+        <location filename="../mainmenu.cpp" line="47"/>
+        <source>Load Game</source>
+        <translation>Load Game</translation>
+    </message>
+    <message>
+        <location filename="../mainmenu.cpp" line="18"/>
+        <location filename="../mainmenu.cpp" line="48"/>
+        <source>Options</source>
+        <translation>Options</translation>
+    </message>
+    <message>
+        <location filename="../mainmenu.cpp" line="19"/>
+        <location filename="../mainmenu.cpp" line="49"/>
+        <source>Credits</source>
+        <translation>Credits</translation>
+    </message>
+    <message>
+        <location filename="../mainmenu.cpp" line="20"/>
+        <location filename="../mainmenu.cpp" line="50"/>
+        <source>Exit</source>
+        <translation>Exit</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>New Game</source>
+        <translation type="vanished">New Game</translation>
+    </message>
+    <message>
+        <source>Load Game</source>
+        <translation type="vanished">Load Game</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation type="vanished">Options</translation>
+    </message>
+    <message>
+        <source>Credits</source>
+        <translation type="vanished">Credits</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation type="vanished">Exit</translation>
+    </message>
+</context>
+<context>
+    <name>OptionsMenu</name>
+    <message>
+        <location filename="../optionsmenu.cpp" line="12"/>
+        <location filename="../optionsmenu.cpp" line="61"/>
+        <source>Resolution</source>
+        <translation>Resolution</translation>
+    </message>
+    <message>
+        <location filename="../optionsmenu.cpp" line="18"/>
+        <location filename="../optionsmenu.cpp" line="62"/>
+        <source>Language</source>
+        <translation>Language</translation>
+    </message>
+    <message>
+        <location filename="../optionsmenu.cpp" line="28"/>
+        <location filename="../optionsmenu.cpp" line="63"/>
+        <source>Back to Menu</source>
+        <translation>Back to Menu</translation>
+    </message>
+</context>
+</TS>
---
+++ code/translations/Projeto_pt_BR.ts
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>MainMenu</name>
+    <message>
+        <source>Tape/Kill</source>
+        <translation type="vanished">Tape/Kill</translation>
+    </message>
+    <message>
+        <location filename="../mainmenu.cpp" line="16"/>
+        <location filename="../mainmenu.cpp" line="46"/>
+        <source>New Game</source>
+        <translation>Novo Jogo</translation>
+    </message>
+    <message>
+        <location filename="../mainmenu.cpp" line="17"/>
+        <location filename="../mainmenu.cpp" line="47"/>
+        <source>Load Game</source>
+        <translation>Carregar Jogo</translation>
+    </message>
+    <message>
+        <location filename="../mainmenu.cpp" line="18"/>
+        <location filename="../mainmenu.cpp" line="48"/>
+        <source>Options</source>
+        <translation>Opções</translation>
+    </message>
+    <message>
+        <location filename="../mainmenu.cpp" line="19"/>
+        <location filename="../mainmenu.cpp" line="49"/>
+        <source>Credits</source>
+        <translation>Créditos</translation>
+    </message>
+    <message>
+        <location filename="../mainmenu.cpp" line="20"/>
+        <location filename="../mainmenu.cpp" line="50"/>
+        <source>Exit</source>
+        <translation>Sair</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Tape/Kill</source>
+        <translation type="obsolete">Tape/Kill</translation>
+    </message>
+</context>
+<context>
+    <name>OptionsMenu</name>
+    <message>
+        <location filename="../optionsmenu.cpp" line="12"/>
+        <location filename="../optionsmenu.cpp" line="61"/>
+        <source>Resolution</source>
+        <translation>Resolução</translation>
+    </message>
+    <message>
+        <location filename="../optionsmenu.cpp" line="18"/>
+        <location filename="../optionsmenu.cpp" line="62"/>
+        <source>Language</source>
+        <translation>Idioma</translation>
+    </message>
+    <message>
+        <location filename="../optionsmenu.cpp" line="28"/>
+        <location filename="../optionsmenu.cpp" line="63"/>
+        <source>Back to Menu</source>
+        <translation>Voltar ao Menu</translation>
+    </message>
+</context>
+</TS>
